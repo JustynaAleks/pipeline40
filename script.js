e8e227787904712cf9ed4728f0f3b06cf330ecce
Justyna <120050900+JustynaAleks@users.noreply.github.com>
2026-03-15 11:01:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-setTimeout(() => {
+const images = [
 
-document.getElementById("loading-screen").style.display="none";
-document.getElementById("main-content").style.display="block";
+"images/marek (1).jpg",
+"images/marek (2).jpg",
+"images/marek (3).jpg",
+"images/marek (4).jpg",
+"images/marek (5).jpg",
+"images/marek (6).jpg",
+"images/marek (7).jpg",
+"images/marek (8).jpg",
+"images/marek (9).jpg",
+"images/marek (10).jpg",
+"images/marek (11).jpg",
+"images/marek (12).jpg",
+"images/marek (13).jpg",
+"images/marek (14).jpg",
+"images/marek (15).jpg",
+"images/marek (16).jpg",
+"images/marek (17).jpg",
+"images/marek (18).jpg",
+"images/marek (19).jpg",
+"images/marek (20).jpg",
+"images/marek (21).jpg",
+"images/marek (22).jpg",
+"images/marek (23).jpg",
+"images/marek (24).jpg",
+"images/marek (25).jpg",
+"images/marek (26).jpg",
+"images/marek (27).jpg",
+"images/marek (28).jpg",
+"images/marek (29).jpg"
 
-},3000);
+]
 
+function randomMemory(){
 
-const memories = [
+let random = Math.floor(Math.random()*images.length)
 
-"Integration that was supposed to end at 22:00",
-"When the project finally worked",
-"That one meeting",
-"Best team moment",
-"Coffee break that lasted 2 hours",
-"Legendary office moment"
+document.getElementById("randomImage").src = images[random]
 
-];
-
-document.getElementById("queryBtn").onclick = function(){
-
-let random = memories[Math.floor(Math.random()*memories.length)];
-
-document.getElementById("queryResult").innerText = random;
-
-};
+}
